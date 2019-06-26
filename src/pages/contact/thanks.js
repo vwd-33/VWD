94b9e6931987fec6a313cccfbd1b207b3a7f9b42
Steve <sameyer17@gmail.com>
2019-06-26 00:20:24 -0400

    thank you fixes

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -5,8 +5,8 @@ export default () => (
   <Layout>
     <section className="section">
       <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
+        <div className="content thankyoupage">
+          <h1 className="thankYou__text">Thank you!</h1>
           <p>This is a custom thank you page for form submissions</p>
         </div>
       </div>
