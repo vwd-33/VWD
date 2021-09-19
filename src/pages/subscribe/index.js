fe81297e6620b4f9052de92f3f2bb8033def5a73
steve <steve@volence.dev>
2021-09-19 16:49:20 -0400

    fix subscription form

--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -3,12 +3,12 @@ import Layout from '../../components/Layout'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import jsonp from "jsonp"
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 const Subscribe = () => {
 	const [email, setEmail] = useState('');
-	const [status, setStatus] = useState('');
 	const [validEmail, setValidEmail] = useState(true);
+	const url = 'https://bigcartel.us18.list-manage.com/subscribe/post?u=2f28a1660890d2f972dc09983&amp;id=56aa869c01'
 
 	const updateAndValidateEmail = (e) => {
 		//eslint-disable-next-line
@@ -16,21 +16,10 @@ const Subscribe = () => {
 		setValidEmail(regex.test(e.target.value));
 		setEmail(e.target.value);
 	}
-	const sendData = async (e) => {
+	const submit = (e, subscribe)=> {
+		console.log('e', e)
 		e.preventDefault();
-		setStatus('Sending...')
-		const url = `https://bigcartel.us18.list-manage.com/subscribe/post-json?u=2f28a1660890d2f972dc09983&amp;id=56aa869c01&email=${email}`
-		jsonp(url, { param: "c" }, (err, data) => {
-			if (data.msg.includes("already subscribed")) {
-				setStatus('Sorry, you\'re already subscribed!')
-			} else if (err) {
-				setStatus('Error, please try again!')
-			} else if (data.result !== 'success') {
-				setStatus('Error, please try again!')
-			} else {
-				setStatus('Subscribed!')
-			};
-		  });
+		subscribe({'EMAIL': email})
 	}
 	return( 
 	<Layout>
@@ -53,41 +42,42 @@ const Subscribe = () => {
           	<div className="container">
 				<div className="content contact-page">
 					<div id="mc_embed_signup">
-						<Box
-							component="form"
-							noValidate
-							autoComplete="off"
-							onSubmit={sendData}
-							className="validate contact__form"
-						>
-							<div id="mc_embed_signup_scroll">
-								<div className="field">
-									<label htmlFor="mce-EMAIL">Subscribe</label>
-								</div>
-								<div className="field control">
-									<TextField 
-										label="Email Address" 
-										variant="filled" 
-										onChange={updateAndValidateEmail} 
-										value={email} 
-										type="email" 
-										fullWidth
-										error={!validEmail}
-										sx={{
-											backgroundColor: "white",
-										}}
-									/>
-								</div>
-								{/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
-								<div id="false-mail" aria-hidden="true"><input type="text" name="b_2f28a1660890d2f972dc09983_56aa869c01" tabIndex="-1" defaultValue="" /></div>
-								<div className="clear field">
-									<Button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button is-link" >Submit</Button>
-								</div>
-							</div>
-						</Box>
+						<MailchimpSubscribe url={url}
+							render={({ subscribe, status, message }) => (
+								<Box
+									component="form"
+									noValidate
+									autoComplete="off"
+									onSubmit={(e) => submit(e, subscribe)}
+									className="validate contact__form"
+								>
+									{/* <CustomForm onSubmitted={formData => subscribe(formData)} /> */}
+									<div className="field control">
+										<TextField 
+											label="Email Address" 
+											variant="filled" 
+											onChange={updateAndValidateEmail} 
+											value={email} 
+											type="email" 
+											fullWidth
+											error={!validEmail}
+											sx={{
+												backgroundColor: "white",
+											}}
+										/>
+									</div>
+									<div className="clear field">
+										<Button type="submit" value="Subscribe" name="subscribe" className="button is-link" disabled={!validEmail}>Submit</Button>
+									</div>
+									{status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+									{status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+									{status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+								</Box>
+
+							)}
+						/>
 					</div>
 				</div>
-				{status}
 			</div>
 		</section>
 	</Layout>
